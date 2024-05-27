1524a594e4da9f602e2cd12cd1c8c0f3e701a735
ohDASEUL <comeseul312@gmail.com>
2024-05-27 20:27:09 +0900

    useParams 적용

--- src/Pages/ProductDetailPage.js
+++ src/Pages/ProductDetailPage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
+  const {id} = useParams()
+  
   return (
     <div>
-      <h1>Product Detail Page</h1>
+      <h1>Product Detail Page{id}</h1>
     </div>
   );
 };
