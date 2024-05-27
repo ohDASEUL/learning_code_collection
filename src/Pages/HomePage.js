9bf78b2ecaba25b5db3099db570d1d4e2f4ce56d
ohDASEUL <comeseul312@gmail.com>
2024-05-27 20:42:17 +0900

    useSearchParams 적용

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+  const navigate=useNavigate()
+  const goProductPage=()=>{
+    navigate('/products?q=pants')
+  }
   return (
     <div>
         <h1>Home Page</h1>
         <Link to="/about">Go to about page</Link>
+        <button onClick={goProductPage}>go to product page</button>
     </div>
   )
 }
--- src/Pages/ProductpPage.js
+++ src/Pages/ProductpPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const ProductpPage = () => {
+  let [query, setQuery] = useSearchParams()
+  console.log("어쩌고저쩌고",query.get("page"))
   return (
     <div>
         <h1>Show All Product</h1>
