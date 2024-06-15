e984b282a89b5d52b2676995a86da3ba451da803
ohDASEUL <comeseul312@gmail.com>
2024-06-15 20:24:29 +0900

    mui 이용해서 메인 상품 페이지 제작

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
+  const navigate = useNavigate();
+  const goProductDetail = () => {
+    navigate(`/product/${item.id}`);
+  };
+
   return (
-    <div>ProductCard</div>
-  )
-}
+    <div className='card-section' onClick={goProductDetail}>
+      <img src={item?.img} alt={item?.title}/>
+      <div>{item?.title}</div>
+      <div>￦{item?.price.toLocaleString()}원</div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- src/Pages/ProuctAllPage.jsx
+++ src/Pages/ProuctAllPage.jsx
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import ProductCard from '../Components/ProductCard'
+import React, { useEffect, useState } from "react";
+import ProductCard from "../Components/ProductCard";
+import { Container, Grid } from "@mui/material";
 
 const ProuctAllPage = () => {
-  const [productList, setProductList] = useState([])
-  const getProducts=async()=>{
-    let url = `http://localhost:5000/products`
-    let res = await fetch(url)
-    let data = await res.json()
-    setProductList(data)
-  }
-  useEffect(()=>{
-    getProducts()
-  },[])
+  const [productList, setProductList] = useState([]);
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    let res = await fetch(url);
+    let data = await res.json();
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div>
-      <ProductCard/>
+      <Container>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 6, md: 10 }}
+        >
+          {productList.map((item) => (
+            <Grid item xs={2} sm={2} md={2} key={item.id}>
+              <ProductCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default ProuctAllPage
+export default ProuctAllPage;
