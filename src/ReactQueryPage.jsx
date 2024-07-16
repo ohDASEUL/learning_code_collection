f43687fc4ade45977d38e0975cf46ff78b265265
ohDASEUL <comeseul312@gmail.com>
2024-07-17 00:00:13 +0900

    기타 옵션

--- src/ReactQueryPage.jsx
+++ src/ReactQueryPage.jsx
@@ -7,15 +7,24 @@ const ReactQueryPage = () => {
   const fetchPost = () => {
     return axios.get("http://localhost:3004/posts");
   };
-  const { isLoading, data, isError, error } = useQuery({
+  const { isLoading, data, isError, error,refetch } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPost,
     retry: 1,
     select: (data) => {
       return data.data;
     },
-    staleTime:60000, // 기본값 0
-    gcTime:10000,
+    enabled:false
+    // 초기에 호출 안 함(기본값 true)
+    // refetchOnWindowFocus:true
+    // 알아서 api를 호출해줘서 항상 최신 데이터를 볼 수 있음
+    // refetchOnMount:true,
+    // compont를 다시 들어갈때 api를 호출 할거냐 말거냐(기본값 true)
+    // refetchInterval:3000
+    // 3초마다 api 호출
+    // staleTime:60000, 
+    // 기본값 0
+    // gcTime:10000,
     // staleTime < gctime
   });
   console.log("ddd", data, isLoading);
@@ -42,9 +51,10 @@ const ReactQueryPage = () => {
           </li>
         </ul>
       </nav>
-      {data.map((item) => (
+      {data?.map((item) => (
         <div>{item.title}</div>
       ))}
+      <button onClick={refetch}>post리스트 다시 들고오기</button>
       
     </div>
   );
