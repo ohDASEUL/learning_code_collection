fb7d40cc60a1a025a85efd0f0c1f819e57eff14a
ohDASEUL <comeseul312@gmail.com>
2024-07-16 22:34:52 +0900

    초기세팅

---
+++ src/ReactQueryPage.jsx
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+
+const ReactQueryPage = () => {
+  const fetchPost = () => {
+    return axios.get("http://localhost:3004/posts");
+  };
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPost,
+    retry: 1,
+    select:(data)=>{
+        return data.data
+    }
+  });
+  console.log("ddd", data, isLoading);
+  console.log("error", isError, error);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  return (
+    <div>
+      {data.map((item) => (
+        <div>{item.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default ReactQueryPage;
