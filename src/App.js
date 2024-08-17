d41261c1f4669eee65fcc65588771e07ea44ac84
ohDASEUL <comeseul312@gmail.com>
2024-08-17 17:29:48 +0900

    폴더/파일 생성 및 라우터세팅

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import HomePage from "./pages/HomePage/HomePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          {/* index <- path="/" 를 그대로 사용하겠다는 뜻 */}
+          <Route index element={<HomePage />} />
+          <Route path="/movies">
+            <Route index element={<MoviePage />} />
+            <Route path=":id" element={<MovieDetailPage />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
