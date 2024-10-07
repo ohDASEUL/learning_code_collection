7c4aa5e8b18c56a5535b20af83bd792766383ca5
ohDASEUL <comeseul312@gmail.com>
2024-10-07 23:55:22 +0900

    [socket] - socket.io 기본 채팅 앱 : 서버 설정 및 실시간 통신 기능 추가...

--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const { join } = require('node:path');
 
 const app = express();
 const server = createServer(app); // 앱을 http 서버로 초기화
 
 app.get('/', (req, res) => { // 웹사이트 홈('/') 경로에 대한 GET 요청 처리기 정의
-  res.send('<h1>Hello world</h1>'); // 클라이언트에 'Hello world' 응답을 보냄
+    res.sendFile(join(__dirname, 'index.html')); // 클라이언트에 'index.html' 파일을 응답으로 보냄
 });
 
 server.listen(3000, () => { // HTTP 서버가 포트 3000에서 요청을 수신하도록 설정
