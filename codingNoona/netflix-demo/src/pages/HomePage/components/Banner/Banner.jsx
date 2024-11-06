025450ee7542026f9c9924e2afe639df00473df7
ohDASEUL <comeseul312@gmail.com>
2024-11-06 17:52:05 +0900

    [코딩알려주는누나-넷플릭스] 배너 디자인하기

--- codingNoona/netflix-demo/src/pages/HomePage/components/Banner/Banner.jsx
+++ codingNoona/netflix-demo/src/pages/HomePage/components/Banner/Banner.jsx
@@ -1,13 +1,37 @@
 import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import Swal from "sweetalert2";
+import "./Banner.style.css";
 
 const Banner = () => {
   const { data, isLoading, error } = usePopularMoviesQuery();
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error:{error.message}</div>;
+  if (error) {
+    Swal.fire({
+      title: "Error",
+      text: `데이터를 불러오는데 실패했습니다: ${error.message}`,
+      icon: "error",
+    });
+    return null;
+  }
   console.log("Popular movies data:", data);
-  return <div>Banner</div>;
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces/${data.results[2].poster_path}` +
+          ")",
+      }}
+      className="banner"
+    >
+      <div className="banner-text-area">
+        <h1>{data.results[0].title}</h1>
+        <p>{data.results[0].overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
