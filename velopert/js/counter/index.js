3c59c0a9e01c2aee948eaf8e00c7fcbbff03bf04
ohDASEUL <comeseul312@gmail.com>
2024-11-11 16:17:41 +0900

    [벨로퍼트와 함께하는 모든 자바스크립트- HTML 과 JS 연동하기] - 이벤트 설정하기 1

--- velopert/js/counter/index.js
+++ velopert/js/counter/index.js
@@ -5,3 +5,11 @@ const decrease = document.getElementById("decrease");
 console.log(number.innerText); // 내용
 console.log(increase.offsetTop); // top 위치
 console.log(decrease.id); // id
+
+increase.onclick = () => {
+  console.log("increase 가 클릭됨");
+};
+
+decrease.onclick = () => {
+  console.log("decrease 가 클릭됨");
+};
