8543d36ba8aa4b516239c07df29a3d8b7f313806
ohDASEUL <comeseul312@gmail.com>
2024-11-11 16:16:35 +0900

    [벨로퍼트와 함께하는 모든 자바스크립트- HTML 과 JS 연동하기] - 카운터_UI 구성하기, DOM 선택하기

---
+++ velopert/js/counter/index.js
@@ -0,0 +1,7 @@
+const number = document.getElementById("number");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+
+console.log(number.innerText); // 내용
+console.log(increase.offsetTop); // top 위치
+console.log(decrease.id); // id
