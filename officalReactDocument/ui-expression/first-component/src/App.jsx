522fe632377f14ccc8a69d9838e8ff31cd988af5
ohDASEUL <comeseul312@gmail.com>
2024-11-18 16:39:18 +0900

    [리액트 공식문서 학습-ui표현하기] :첫번째 컴포넌트 - 컴포넌트 사용하기

--- officalReactDocument/ui-expression/first-component/src/App.jsx
+++ officalReactDocument/ui-expression/first-component/src/App.jsx
@@ -1,8 +1,15 @@
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+}
+
 function App() {
   return (
-    <div>
-      <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
-    </div>
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
   );
 }
 
