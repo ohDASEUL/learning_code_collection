1e8223457693261671afc727d61046ac19a1a969
ohDASEUL <comeseul312@gmail.com>
2024-11-19 23:22:12 +0900

    [리액트 공식문서 학습-ui표현하기] : 조건부 렌더링 - 챌린지 2 of 3

--- officalReactDocument/ui-expression/conditional-rendering/src/App.jsx
+++ officalReactDocument/ui-expression/conditional-rendering/src/App.jsx
@@ -1,31 +1,43 @@
-// 챌린지 1 of 3
-// ? :를 사용하여 완료되지 않은 항목의 아이콘을 표시
-// isPacked가 true가 아닌 경우 조건부 연산자(cond ? a : b)를 사용하여 ❌를 렌더링
-// function Item({ name, isPacked }) {
+// 챌린지 2 of 3
+// 각 Item은 숫자 타입인 importance를 props로 받고 있음.
+// & 연산자를 사용하여 “(Importance: X)“를 이탤릭체로 렌더링하되 난이도가 0이 아닌 항목만 렌더링
+
+// function Item({ name, importance }) {
 //   return (
 //     <li className="item">
-//       {name} {isPacked && "✅"}
+//       {name}
 //     </li>
 //   );
 // }
 
 // 작성 코드
-function Item({ name, isPacked }) {
+function Item({ name, importance }) {
   return (
     <li className="item">
-      {name} {isPacked ? "✅" : "❌"}
+      {name} {importance > 0 && <i>(Importance: {importance})</i>}
     </li>
   );
 }
+
+// 해설 코드
+// function Item({ name, importance }) {
+//   return (
+//     <li className="item">
+//       {name}
+//       {importance > 0 && " "}
+//       {importance > 0 && <i>(Importance: {importance})</i>}
+//     </li>
+//   );
+// }
 
 function App() {
   return (
     <section>
       <h1>Sally Ride`s Packing List</h1>
       <ul>
-        <Item isPacked={true} name="Space suit" />
-        <Item isPacked={true} name="Helmet with a golden leaf" />
-        <Item isPacked={false} name="Photo of Tam" />
+        <Item importance={9} name="Space suit" />
+        <Item importance={0} name="Helmet with a golden leaf" />
+        <Item importance={6} name="Photo of Tam" />
       </ul>
     </section>
   );
