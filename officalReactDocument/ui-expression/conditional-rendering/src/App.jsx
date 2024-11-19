e684758c2924cc940ae495772e7a90b7d30e9afb
ohDASEUL <comeseul312@gmail.com>
2024-11-19 22:29:45 +0900

    [리액트 공식문서 학습-ui표현하기] : 조건부 렌더링 - 조건부로 JSX 포함시키기(논리 AND 연산자)

--- officalReactDocument/ui-expression/conditional-rendering/src/App.jsx
+++ officalReactDocument/ui-expression/conditional-rendering/src/App.jsx
@@ -1,6 +1,8 @@
 function Item({ name, isPacked }) {
   return (
-    <li className="item">{isPacked ? <del>{name + " ✅"}</del> : name}</li>
+    <li className="item">
+      {name} {isPacked && "✅"}
+    </li>
   );
 }
 
