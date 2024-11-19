99df9ba098bdb75b958ac1294d06fc6dd079fb16
ohDASEUL <comeseul312@gmail.com>
2024-11-19 23:02:39 +0900

    [리액트 공식문서 학습-ui표현하기] : 조건부 렌더링 - 챌린지 1 of 3

--- officalReactDocument/ui-expression/conditional-rendering/src/App.jsx
+++ officalReactDocument/ui-expression/conditional-rendering/src/App.jsx
@@ -1,9 +1,21 @@
+// 챌린지 1 of 3
+// ? :를 사용하여 완료되지 않은 항목의 아이콘을 표시
+// isPacked가 true가 아닌 경우 조건부 연산자(cond ? a : b)를 사용하여 ❌를 렌더링
+// function Item({ name, isPacked }) {
+//   return (
+//     <li className="item">
+//       {name} {isPacked && "✅"}
+//     </li>
+//   );
+// }
+
+// 작성 코드
 function Item({ name, isPacked }) {
-  let itemContent = name;
-  if (isPacked) {
-    itemContent = <del>{name + " ✅"}</del>;
-  }
-  return <li className="item">{itemContent}</li>;
+  return (
+    <li className="item">
+      {name} {isPacked ? "✅" : "❌"}
+    </li>
+  );
 }
 
 function App() {
