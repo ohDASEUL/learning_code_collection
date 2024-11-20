9754b45c21f26790d8674b6f6182cb0e29ac65a8
ohDASEUL <comeseul312@gmail.com>
2024-11-20 21:36:34 +0900

    [벨로퍼트와 함께하는 모던 리액트 - 배열에 항목 수정하기] - onToggle 함수 생성 후 UserList 컴포넌트에게 전달

--- velopert/react/begin-react/src/App.js
+++ velopert/react/begin-react/src/App.js
@@ -58,6 +58,14 @@ function App() {
     // = user.id 가 id 인 것을 제거함
     setUsers(users.filter((user) => user.id !== id));
   };
+
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
   return (
     <>
       <CreateUser
@@ -66,7 +74,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
