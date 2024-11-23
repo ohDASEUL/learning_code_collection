0b55d0996a097769820af07e73d5a498b7fdadbf
ohDASEUL <comeseul312@gmail.com>
2024-11-23 16:54:16 +0900

    [리액트 공식문서 학습-상호작용성 더하기] : state 컴포넌트의 기억 저장소_챌린지 4 of 4 해설 코드

--- officalReactDocument/interactionability_plus/state/src/App.jsx
+++ officalReactDocument/interactionability_plus/state/src/App.jsx
@@ -7,7 +7,11 @@
 
 // 이 코드를 수정하려면 불필요한 state 변수를 제거
 
-// 작성한 코드
+// 해설 코드(작성한 코드랑 같음)
+
+// 컴포넌트가 다시 렌더링 될 때만 정보를 유지하기 위해 state 변수가 필요
+
+// 단일 이벤트 핸들러 내에서는 일반 변수가 잘 작동하지만 일반 변수가 잘 동작할 때 state 변수를 도입할 필요는 없음
 
 export default function FeedbackForm() {
   function handleClick() {
