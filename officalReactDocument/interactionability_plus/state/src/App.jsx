41b276c2243d535fd85f7976fb0f54e5e13476cd
ohDASEUL <comeseul312@gmail.com>
2024-11-23 15:49:43 +0900

    [리액트 공식문서 학습-상호작용성 더하기] : state 컴포넌트의 기억 저장소_챌린지 1 of 4

--- officalReactDocument/interactionability_plus/state/src/App.jsx
+++ officalReactDocument/interactionability_plus/state/src/App.jsx
@@ -1,13 +1,42 @@
-import Gallery from "./Gallery";
-import "./App.css";
+import { useState } from "react";
+import { sculptureList } from "./data";
+
+// 챌린지 1 of 4
+
+// 마지막 조각상에서 “Next”를 누르면 코드가 충돌.. 로직을 수정하여 이를 해결
+// 이벤트 핸들러에 추가로 로직을 추가하거나 동작이 불가능할 때 버튼을 비활성화하여 이를 처리 가능
+// 충돌을 수정한 후, 이전 조각상을 표시하는 “Previous” 버튼을 추가
+// 첫 번째 조각상에서는 충돌이 발생하지 않아야 함
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <div className="Page">
-      <Gallery />
-      <Gallery />
-    </div>
+    <>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
   );
 }
-
 export default App;
