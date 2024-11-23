36f46ff5f3459d89e2a82c37521d6b8b35bb75f1
ohDASEUL <comeseul312@gmail.com>
2024-11-23 14:20:06 +0900

    [리액트 공식문서 학습-상호작용성 더하기] : state 컴포넌트의 기억 저장소_컴포넌트에 여러 state 변수 저장하기

--- officalReactDocument/interactionability_plus/state/src/App.jsx
+++ officalReactDocument/interactionability_plus/state/src/App.jsx
@@ -3,9 +3,14 @@ import { useState } from "react";
 
 function App() {
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
   function handleClick() {
     setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
@@ -19,6 +24,10 @@ function App() {
       <h3>
         ({index + 1}of{sculptureList.length})
       </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"}details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
       <img src={sculpture.url} alt={sculpture.alt} />
       <p>{sculpture.description}</p>
     </>
