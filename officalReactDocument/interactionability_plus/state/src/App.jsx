1cc835f6124c4998fc209e3509b3b9deeacccfff
ohDASEUL <comeseul312@gmail.com>
2024-11-23 16:25:11 +0900

    [리액트 공식문서 학습-상호작용성 더하기] : state 컴포넌트의 기억 저장소_챌린지 3 of 4

--- officalReactDocument/interactionability_plus/state/src/App.jsx
+++ officalReactDocument/interactionability_plus/state/src/App.jsx
@@ -1,49 +1,35 @@
-// 챌린지 2 of 4
+// 챌린지 3 of 4
 
-// 폼 입력 불가 문제 고치기
+// 충돌 고치기
 
-// 입력 필드에 입력하면 아무것도 나타나지 않음
-// 마치 입력값이 빈 문자열로 “고정”된 것처럼 보임
-// 두 입력 모두 onChange 이벤트 핸들러를 가지고 있으며, 최신 사용자 입력(e.target.value)을 기반으로 변수를 업데이트하려고 시도
-// 그러나 변수들은 다시 렌더링 되는 동안 값을 “기억”하지 않는 것처럼 보임
-// state 변수를 사용하여 이 문제를 해결
+// 사용자가 피드백을 남길 수 있는 간단한 폼이 있는데, 피드백을 제출하면 감사 메시지가 표시되어야 함
+// 그러나 “예상보다 적은 훅을 렌더링했습니다”라는 오류 메시지와 함께 충돌이 발생
 
-// 해설 코드(작성한 코드랑 같음)
 import { useState } from "react";
 
-export default function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
-  function handleFirstNameChange(e) {
-    setFirstName(e.target.value);
+export default function FeedbackForm() {
+  const [isSent, setIsSent] = useState(false);
+  if (isSent) {
+    return <h1>Thank you!</h1>;
+  } else {
+    // eslint-disable-next-line
+    const [message, setMessage] = useState("");
+    return (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          alert(`Sending: "${message}"`);
+          setIsSent(true);
+        }}
+      >
+        <textarea
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <br />
+        <button type="submit">Send</button>
+      </form>
+    );
   }
-
-  function handleLastNameChange(e) {
-    setLastName(e.target.value);
-  }
-
-  function handleReset() {
-    setFirstName("");
-    setLastName("");
-  }
-
-  return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <input
-        placeholder="First name"
-        value={firstName}
-        onChange={handleFirstNameChange}
-      />
-      <input
-        placeholder="Last name"
-        value={lastName}
-        onChange={handleLastNameChange}
-      />
-      <h1>
-        Hi, {firstName} {lastName}
-      </h1>
-      <button onClick={handleReset}>Reset</button>
-    </form>
-  );
 }
