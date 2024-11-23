b87d213db38103c28f28ebd8a638f015974f5062
ohDASEUL <comeseul312@gmail.com>
2024-11-23 14:08:49 +0900

    [리액트 공식문서 학습-상호작용성 더하기] : state 컴포넌트의 기억 저장소_일반 변수로 충분하지 않은 경우

---
+++ officalReactDocument/interactionability_plus/state/src/App.jsx
@@ -0,0 +1,27 @@
+import { sculptureList } from "./data";
+
+function App() {
+  let index = 0;
+
+  function handleClick() {
+    index = index + 1;
+  }
+
+  let sculpture = sculptureList[index];
+
+  return (
+    <>
+      <button onClick={handleClick}>Next</button>
+      <h2>
+        <i>{sculpture.name}</i> by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1}of{sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
+    </>
+  );
+}
+
+export default App;
