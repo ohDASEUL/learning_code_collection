491b87c6eae9a0b25eb197f4913477a735df5d9c
ohDASEUL <comeseul312@gmail.com>
2024-11-23 16:14:20 +0900

    [리액트 공식문서 학습-상호작용성 더하기] : state 컴포넌트의 기억 저장소_챌린지 2 of 4

--- officalReactDocument/interactionability_plus/state/src/App.jsx
+++ officalReactDocument/interactionability_plus/state/src/App.jsx
@@ -1,59 +1,46 @@
-import { useState } from "react";
-import { sculptureList } from "./data";
+// 챌린지 2 of 4
 
-// 챌린지 1 of 4
+// 폼 입력 불가 문제 고치기
 
-// 마지막 조각상에서 “Next”를 누르면 코드가 충돌.. 로직을 수정하여 이를 해결
-// 이벤트 핸들러에 추가로 로직을 추가하거나 동작이 불가능할 때 버튼을 비활성화하여 이를 처리 가능
-// 충돌을 수정한 후, 이전 조각상을 표시하는 “Previous” 버튼을 추가
-// 첫 번째 조각상에서는 충돌이 발생하지 않아야 함
+// 입력 필드에 입력하면 아무것도 나타나지 않음
+// 마치 입력값이 빈 문자열로 “고정”된 것처럼 보임
+// 두 입력 모두 onChange 이벤트 핸들러를 가지고 있으며, 최신 사용자 입력(e.target.value)을 기반으로 변수를 업데이트하려고 시도
+// 그러나 변수들은 다시 렌더링 되는 동안 값을 “기억”하지 않는 것처럼 보임
+// state 변수를 사용하여 이 문제를 해결
 
-// 해설 코드
+export default function App() {
+  let firstName = "";
+  let lastName = "";
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
-
-  let hasPrev = index > 0;
-  let hasNext = index < sculptureList.length - 1;
-
-  function handlePrevClick() {
-    if (hasPrev) {
-      setIndex(index - 1);
-    }
+  function handleFirstNameChange(e) {
+    firstName = e.target.value;
   }
 
-  function handleNextClick() {
-    if (hasNext) {
-      setIndex(index + 1);
-    }
+  function handleLastNameChange(e) {
+    lastName = e.target.value;
   }
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
+  function handleReset() {
+    firstName = "";
+    lastName = "";
   }
 
-  let sculpture = sculptureList[index];
   return (
-    <>
-      <button onClick={handlePrevClick} disabled={!hasPrev}>
-        Previous
-      </button>
-      <button onClick={handleNextClick} disabled={!hasNext}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? "Hide" : "Show"} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
-    </>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>
+        Hi, {firstName} {lastName}
+      </h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
   );
 }
