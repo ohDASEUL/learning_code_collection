8a796c6a0527fd6c5b7240723b82a3405077d9ad
ohDASEUL <comeseul312@gmail.com>
2024-11-25 17:19:25 +0900

    [리액트 공식문서 학습-state 관리하기] - state를 사용해 input 다루기_ 많은 시각적 state를 한 번에 보여주기

--- officalReactDocument/state_managing/input/src/App.jsx
+++ officalReactDocument/state_managing/input/src/App.jsx
@@ -1,23 +1,16 @@
-function App({ status = "success" }) {
-  if (status === "success") {
-    return <h1>That`s right!</h1>;
-  }
+import Form from "./Form.jsx";
+
+let statuses = ["empty", "typing", "submitting", "success", "error"];
+
+function App() {
   return (
     <>
-      <h2>City quiz</h2>
-      <p>
-        In which city is there a billboard that turns air into drinkable water?
-      </p>
-      <form>
-        <textarea disabled={status === "submitting"} />
-        <br />
-        <button disabled={status === "empty" || status === "submitting"}>
-          Submit
-        </button>
-        {status === "error" && (
-          <p className="Error">Good guess but a wrong answer. Try again!</p>
-        )}
-      </form>
+      {statuses.map((status) => (
+        <section key={status}>
+          <h4>Form ({status}):</h4>
+          <Form status={status} />
+        </section>
+      ))}
     </>
   );
 }
