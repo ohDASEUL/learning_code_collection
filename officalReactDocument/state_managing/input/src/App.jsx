4d7a2cd080d6df0931da189f3a1014c3e013bb2d
ohDASEUL <comeseul312@gmail.com>
2024-11-25 17:40:02 +0900

    [리액트 공식문서 학습-state 관리하기] - state를 사용해 input 다루기_ state 설정을 위해 이벤트 핸들러를 연결하기

--- officalReactDocument/state_managing/input/src/App.jsx
+++ officalReactDocument/state_managing/input/src/App.jsx
@@ -1,16 +1,62 @@
-import Form from "./Form.jsx";
+import { useState } from "react";
 
-let statuses = ["empty", "typing", "submitting", "success", "error"];
+function submitForm(answer) {
+  // 네트워크에 접속한다고 가정
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== "lima";
+      if (shouldError) {
+        reject(new Error("Good guess but a wrong answer. Try again!"));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
 
 function App() {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
+
+  if (status === "success") {
+    return <h1>That`s right!</h1>;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+    } catch (err) {
+      setStatus("typing");
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+  }
+
   return (
     <>
-      {statuses.map((status) => (
-        <section key={status}>
-          <h4>Form ({status}):</h4>
-          <Form status={status} />
-        </section>
-      ))}
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === "submitting"}
+        />
+        <br />
+        <button disabled={answer.length === 0 || status === "submitting"}>
+          Submit
+        </button>
+        {error !== null && <p className="Error">{error.message}</p>}
+      </form>
     </>
   );
 }
