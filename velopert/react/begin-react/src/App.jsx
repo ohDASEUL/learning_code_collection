633fa422a6e9c5ece61981e31733a9b73ee5687d
ohDASEUL <comeseul312@gmail.com>
2024-11-27 17:21:51 +0900

    [벨로퍼트와 함께하는 모던 리액트-useMemo 를 사용하여 연산한 값 재사용하기] - countActiveUsers 라는 함수를 만들어서, active 값이 true 인 사용자의 수를 세어서 화면에 렌더링

--- velopert/react/begin-react/src/App.jsx
+++ velopert/react/begin-react/src/App.jsx
@@ -2,6 +2,11 @@ import React, { useRef, useState } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 
+function countActiveUsers(users) {
+  console.log("활성 사용자 수를 세는중...");
+  return users.filter((user) => user.active).length;
+}
+
 function App() {
   const [inputs, setInputs] = useState({
     username: "",
@@ -66,6 +71,9 @@ function App() {
       )
     );
   };
+
+  const count = countActiveUsers(users);
+
   return (
     <>
       <CreateUser
@@ -75,6 +83,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
