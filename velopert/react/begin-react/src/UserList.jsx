caa433f0dcd372223f8736018bcacf7bf65093ea
ohDASEUL <comeseul312@gmail.com>
2024-11-27 17:16:44 +0900

    [벨로퍼트와 함께하는 모던 리액트-useEffect를 사용해 마운트/언마운트/업데이트 시 할 작업 설정] - deps 에 특정 값 넣기

--- velopert/react/begin-react/src/UserList.jsx
+++ velopert/react/begin-react/src/UserList.jsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
   useEffect(() => {
+    console.log("user 값이 설정됨");
     console.log(user);
-  });
-
+    return () => {
+      console.log("user 가 바뀌기 전..");
+      console.log(user);
+    };
+  }, [user]);
   return (
     <div>
       <b
-        style={{ cursor: "pointer", color: User.active ? "green" : "black" }}
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
         onClick={() => onToggle(user.id)}
       >
         {user.username}
