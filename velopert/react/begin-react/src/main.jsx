7362949ba31dd3607bb1d9e090d9511bcf5e77f4
ohDASEUL <comeseul312@gmail.com>
2024-11-28 17:27:32 +0900

    [벨로퍼트와 함께하는 모던 리액트- useReducer 를 사용하여 상태 업데이트 로직 분리하기] - App 컴포넌트를 useReducer 로 구현하기

--- velopert/react/begin-react/src/main.jsx
+++ velopert/react/begin-react/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import Counter from "./Counter.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Counter />
+    <App />
   </StrictMode>
 );
