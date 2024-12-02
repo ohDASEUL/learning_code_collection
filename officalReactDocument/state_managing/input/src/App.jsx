445521a90dadcf8429bd483ae7d21bcac693104a
ohDASEUL <comeseul312@gmail.com>
2024-12-02 17:35:57 +0900

    [리액트 공식문서 학습-state 관리하기] - state를 사용해 input 다루기_ 챌린지 1 of 3 해설 코드

--- officalReactDocument/state_managing/input/src/App.jsx
+++ officalReactDocument/state_managing/input/src/App.jsx
@@ -3,33 +3,32 @@
 // 사진을 클릭하면 바깥에 있는 <div>의 background--active CSS 클래스를 제거하고 <img>에 picture--active 클래스를 추가
 // 배경을 다시 클릭하면 원래 CSS 클래스로 돌아오기
 
-// 내가 작성한 코드
+// 해설 코드
 
 import { useState } from "react";
 import "./App.css";
 
 function Picture() {
-  const [isClicked, setIsClicked] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-  const handleBackgroundClick = () => {
-    setIsClicked(false); // 배경 클릭 시 항상 false
-  };
-
-  const handleImageClick = (e) => {
-    e.stopPropagation(); // 이벤트 버블링 방지
-    setIsClicked(true); // 이미지 클릭 시 항상 true
-  };
+  let backgroundClassName = "background";
+  let pictureClassName = "picture";
+  if (isActive) {
+    pictureClassName += " picture--active";
+  } else {
+    backgroundClassName += " background--active";
+  }
 
   return (
-    <div
-      className={`background ${isClicked ? "" : "background--active"}`}
-      onClick={handleBackgroundClick}
-    >
+    <div className={backgroundClassName} onClick={() => setIsActive(false)}>
       <img
-        className={`picture ${isClicked ? "picture--active" : ""}`}
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsActive(true);
+        }}
+        className={pictureClassName}
         alt="Rainbow houses in Kampung Pelangi, Indonesia"
         src="https://i.imgur.com/5qwVYb1.jpeg"
-        onClick={handleImageClick}
       />
     </div>
   );
