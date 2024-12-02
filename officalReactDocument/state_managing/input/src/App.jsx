4c4ac4fef5cc4a772b7bcc652c006b7cf99801c5
ohDASEUL <comeseul312@gmail.com>
2024-12-02 17:27:12 +0900

    [리액트 공식문서 학습-state 관리하기] - state를 사용해 input 다루기_ 챌린지 1 of 3 작성한 코드

--- officalReactDocument/state_managing/input/src/App.jsx
+++ officalReactDocument/state_managing/input/src/App.jsx
@@ -3,13 +3,28 @@
 // 사진을 클릭하면 바깥에 있는 <div>의 background--active CSS 클래스를 제거하고 <img>에 picture--active 클래스를 추가
 // 배경을 다시 클릭하면 원래 CSS 클래스로 돌아오기
 
+// 내가 작성한 코드
+
+import { useState } from "react";
+import "./App.css";
+
 function Picture() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
-    <div className="background background--active">
+    <div
+      className={`background ${isClicked ? "" : "background--active"}`}
+      onClick={handleClick}
+    >
       <img
-        className="picture"
+        className={`picture ${isClicked ? "picture--active" : ""}`}
         alt="Rainbow houses in Kampung Pelangi, Indonesia"
         src="https://i.imgur.com/5qwVYb1.jpeg"
+        onClick={handleClick}
       />
     </div>
   );
