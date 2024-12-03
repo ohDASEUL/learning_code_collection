176da96ef7578ef98fb159b484fed97f288862cb
ohDASEUL <comeseul312@gmail.com>
2024-12-03 21:45:33 +0900

    [리액트 공식문서 학습-state 관리하기] - state 구조 선택하기_ 연관된 state 모순 피하기 1

--- officalReactDocument/state_managing/structure/src/App.jsx
+++ officalReactDocument/state_managing/structure/src/App.jsx
@@ -1,42 +1,52 @@
 import { useState } from "react";
 
-function MovingDot() {
-  const [position, setPosition] = useState({
-    x: 0,
-    y: 0,
-  });
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsSending(true);
+    await sendMessage(text);
+    setIsSending(false);
+    setIsSent(true);
+  }
+
+  if (isSent) {
+    return <h1>Thanks for feedback!</h1>;
+  }
+
   return (
-    <div
-      onPointerMove={(e) => {
-        setPosition({
-          x: e.clientX,
-          y: e.clientY,
-        });
-      }}
-      style={{
-        position: "relative",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          backgroundColor: "red",
-          borderRadius: "50%",
-          transform: `translate(${position.x}px, ${position.y}px)`,
-          left: -10,
-          top: -10,
-          width: 20,
-          height: 20,
-        }}
+    <form onSubmit={handleSubmit}>
+      <p>How was your stay at The Prancing Pony?</p>
+      <textarea
+        disabled={isSending}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
       />
-    </div>
+      <br />
+      <button disabled={isSending} type="submit">
+        Send
+      </button>
+      {isSending && <p>Sending...</p>}
+    </form>
   );
 }
 
+function sendMessage(text) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+}
+
 function App() {
-  return <MovingDot></MovingDot>;
+  return (
+    <>
+      <FeedbackForm></FeedbackForm>
+      <sendMessage></sendMessage>
+    </>
+  );
 }
 
 export default App;
